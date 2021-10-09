10bf616698ade88b3f6ee213bd9f0fd1167db588
xingxingRealzyx <1064280153@qq.com>
2021-10-09 18:22:38 +0800

    add filter

---
+++ filter/filter.h
@@ -0,0 +1,13 @@
+#ifndef __FILTER_H__
+#define __FILTER_H__
+
+class Filter {
+
+public:
+    
+
+private:
+
+}
+
+#endif /* __FILTER_H__ */
---
+++ filter/lowpass_filter/lowpass_filter.h
@@ -0,0 +1,17 @@
+#ifndef __LOWPASS_FILTER_H__
+#define __LOWPASS_FILTER_H__
+
+#include <filter.h>
+
+class LowPassFilter : public Filter {
+
+public:
+
+
+private:
+
+
+};
+
+
+#endif /* __LOWPASS_FILTER_H__ */
