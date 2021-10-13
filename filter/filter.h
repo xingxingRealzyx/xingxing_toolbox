8f36b1375b4747d9349e2dd204267446c839e608
xingxingRealzyx <1064280153@qq.com>
2021-10-13 18:13:05 +0800

    add lowpass filter

--- filter/filter.h
+++ filter/filter.h
@@ -1,13 +1,17 @@
 #ifndef __FILTER_H__
 #define __FILTER_H__
 
+#include <stdint.h>
+
+#define PI 3.1415926f
+
 class Filter {
 
 public:
-    
-
+    Filter();
+    ~Filter();
 private:
 
-}
+};
 
 #endif /* __FILTER_H__ */
--- filter/lowpass_filter/lowpass_filter.cpp
+++ filter/lowpass_filter/lowpass_filter.cpp
@@ -1,1 +1,41 @@
+#include <math.h>
 #include <lowpass_filter.h>
+
+
+LowPassFilter::LowPassFilter(float cutFrq, uint32_t sampleFrq)
+{
+    _cutFrq = cutFrq;
+    _sampleFrq = sampleFrq;
+    _cof = 0.0f;
+    _Vo_p = 0;
+    _direct = false;
+}
+
+LowPassFilter::LowPassFilter(float cof)
+{
+    _cof = cof;
+    _Vo_p = 0;
+    _direct = true;
+}
+
+LowPassFilter::~LowPassFilter()
+{
+
+}
+
+void LowPassFilter::run(int16_t *Vi, int16_t *Vo)
+{
+    if (_direct)
+    {
+        *Vo = _cof*(*Vi) + (1-_cof)*_Vo_p; 
+    } else {
+        float RC, Cof1, Cof2;
+	
+        RC = (float)1.0/2.0/PI/_cutFrq;
+        Cof1 = 1/(1+RC*_sampleFrq);
+        Cof2 = RC*_sampleFrq/(1+RC*_sampleFrq);
+        *Vo = Cof1 * (*Vi) + Cof2 * (_Vo_p);	
+    }
+    // update
+    _Vo_p = *Vo;
+}
